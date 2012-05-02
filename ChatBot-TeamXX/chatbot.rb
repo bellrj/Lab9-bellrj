4d0e6ac5180ca3ca9e77f2931b6578efe7108dc1
Ryne Bell <bellrj@rose-hulman.edu>
2012-05-02 18:43:07 -0400

    added choose a game functionality!

--- ChatBot-TeamXX/chatbot.rb
+++ ChatBot-TeamXX/chatbot.rb
@@ -2,3 +2,10 @@ puts 'What is your name?'
 name = gets.chomp
 STDOUT.sync = true
 puts 'Hello, ' + name + '!'
+puts "Pick a game:\n1. Starcraft\n2. Skyrim\n3. Programming Extreme!\n4. Warcraft\n5. Global Thermonuclear War"
+number = gets.chomp
+if number == "5"
+	puts "BOOM!"
+else
+	puts "I refuse to play!"
+end
